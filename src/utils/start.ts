f24b827a7ae79c5f75dfdf6098af1c676309f1ec
Ruvian S <12111730+matricce@users.noreply.github.com>
2023-04-09 11:43:22 -0300

    fix: corrigido problema de configuração de webhook do Telegram ao fazer deploy do projeto na Vercel

--- src/utils/start.ts
+++ src/utils/start.ts
@@ -1,17 +1,6 @@
 import 'dotenv/config';
 import { Bot, Context } from 'grammy';
 
-const production = async (bot: Bot<Context>): Promise<void> => {
-  const webhookUrl = `${process.env.VERCEL_URL}/api/index`;
-  console.log(`Bot starting webhook: ${process.env.BOT_TOKEN?.split(':')[0]} ${webhookUrl}`);
-  return bot.api
-    .setWebhook(webhookUrl)
-    .then(() => {
-      console.log('Bot starting webhook: Success');
-    })
-    .catch(console.error);
-};
-
 const development = async (bot: Bot<Context>): Promise<void> => {
   console.log(`Bot starting polling: ${process.env.BOT_TOKEN?.split(':')[0]}`);
   return bot.api
@@ -23,4 +12,4 @@ const development = async (bot: Bot<Context>): Promise<void> => {
     .catch(console.error);
 };
 
-export { production, development };
+export { development };
